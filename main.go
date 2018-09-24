4014bbf7861cd57b742de4e4071cf54146827f62
Chris Phelps <cphelps@tendrilinc.com>
2018-09-24 15:41:57 -0600

    Basic flag parsing

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	flag "github.com/ogier/pflag"
+)
+
+var (
+	user string
+)
 
 func main() {
-    fmt.Println("Hello, World")
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		fmt.Printf("Usage: %s [options]\n", os.Args[0])
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+    fmt.Printf("Hello, %s\n", user)
+}
+
+func init() {
+	flag.StringVarP(&user, "user", "u", "", "Search Users")
 }
